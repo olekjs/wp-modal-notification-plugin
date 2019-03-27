b38ad4f9bb8b994edf85939f76cd0bf45859956d
Aleksander Kaim <olek_kaim@o2.pl>
2019-03-27 22:15:52 +0100

    Init files

---
+++ includes/class-modal-notification.php
@@ -0,0 +1,38 @@
+<?php
+
+class ModalNotification
+{
+    public function __construct()
+    {
+        add_action('admin_menu', [$this, 'add_page']);
+        // add_action('admin_init', [$this, 'page_init']);
+    }
+
+    public function add_page()
+    {
+        add_options_page(
+            'Settings Admin',
+            'Modal Notification',
+            'manage_options',
+            'modal_notification_settings_page',
+            [$this, 'create_page']
+        );
+    }
+
+    public function create_page() {
+		?>
+			<div class="wrap">
+				<h2>Ustawienia okna z powiadomieniem</h2>           
+				<form method="post" action="options.php">
+					<?php
+						echo 'test'; 
+					?>
+				</form>
+			</div>
+		<?php
+	}
+
+	public function page_init() {        
+		//     
+	}
+}
---
+++ modal-notification.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Modal Notification
+Description: Proste tworzenie okna modalnego z powiadomieniem.
+Version: 1.0
+Author: Olek Kaim
+Author URI: https://olekkaim.pl
+*/
+
+
+require_once plugin_dir_path(__FILE__) . 'includes/class-modal-notification.php';
+
+new ModalNotification();
