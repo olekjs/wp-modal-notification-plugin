8f09f87f9819153f7221821e876e429aa36f5704
Aleksander Kaim <olek_kaim@o2.pl>
2019-03-29 20:01:51 +0100

    Create shit code

--- includes/class-modal-notification.php
+++ includes/class-modal-notification.php
@@ -2,12 +2,54 @@
 
 class ModalNotification
 {
-    public function __construct() {
+    private $options;
+
+    public function __construct()
+    {
+        add_action('wp_head', [$this, 'init_scripts']);
+        add_action('wp_head', [$this, 'init_styles']);
+
         add_action('admin_menu', [$this, 'add_page']);
-        // add_action('admin_init', [$this, 'page_init']);
+        add_action('admin_init', [$this, 'page_init']);
+        add_action('the_content', [$this, 'init_modal']);
     }
 
-    public function add_page() {
+    public function init_modal()
+    {
+        echo '
+	    	<div class="page-wrapper">
+			  <a class="btn trigger" href="javascript:;">Click Me!</a>
+			</div>
+			<div class="modal-wrapper">
+			  <div class="modal">
+			    <div class="head">
+			      <a class="btn-close trigger" href="javascript:;"></a>
+			    </div>
+			    <div class="content">';
+			   	echo get_option('modal_notification_content')['modal_notification_content'];
+			    echo '
+			    </div>
+			  </div>
+			</div>';
+    }
+
+    public function init_styles()
+    {
+        echo '<link rel="stylesheet" href="' . plugins_url('styles/modal-notification.css', __FILE__) . '" type="text/css"/>';
+    }
+
+    public function init_scripts()
+    {
+        if (!wp_script_is('jquery')) {
+
+            echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>';
+        }
+
+        echo '<script type="text/javascript" src="' . plugins_url('scripts/modal-notification.js', __FILE__) . '"></script>';
+    }
+
+    public function add_page()
+    {
         add_options_page(
             'Settings Admin',
             'Modal Notification',
@@ -17,20 +59,68 @@ class ModalNotification
         );
     }
 
-    public function create_page() {
-		?>
+    public function create_page()
+    {
+        $this->options = get_option('modal_notification_content');
+        ?>
 			<div class="wrap">
-				<h2>Ustawienia okna z powiadomieniem</h2>           
+				<h2>Ustawienia okna z powiadomieniem</h2>
 				<form method="post" action="options.php">
 					<?php
-						echo 'test'; 
-					?>
+						settings_fields('modal_notification_group');
+				        do_settings_sections('modal_notification_settings_page');
+				        submit_button();
+			        ?>
 				</form>
 			</div>
 		<?php
 	}
 
-	public function page_init() {        
-		//     
-	}
+    public function page_init()
+    {
+        register_setting(
+            'modal_notification_group',
+            'modal_notification_content',
+            array($this, 'sanitize')
+        );
+
+        add_settings_section(
+            'modal_notification_id',
+            'Edycja okna z powiadomieniem',
+            array( $this, 'section_callback' ),
+            'modal_notification_settings_page'
+        ); 
+
+        add_settings_field(
+            'modal_notification_content',
+            'Kontent',
+            array( $this, 'content_callback' ),
+            'modal_notification_settings_page',
+            'modal_notification_id'          
+        );
+    }
+
+    public function sanitize($input)
+    {
+        $new_input = array();
+
+        if (isset($input['modal_notification_content'])) {
+            $new_input['modal_notification_content'] = sanitize_text_field($input['modal_notification_content']);
+        }
+
+        return $new_input;
+    }
+
+    public function section_callback()
+    {
+        echo '';
+    }
+
+    public function content_callback()
+    {
+         printf(
+            '<textarea id="modal_notification_content" name="modal_notification_content[modal_notification_content]" value="%s"> </textarea>',
+            isset( $this->options['modal_notification_content'] ) ? esc_attr( $this->options['modal_notification_content']) : ''
+        );
+    }
 }
---
+++ includes/scripts/modal-notification.js
@@ -0,0 +1,7 @@
+jQuery(function($) {
+  $('.trigger').click(function() {
+      $('.modal-wrapper').toggleClass('open');
+      $('.page-wrapper').toggleClass('blur');
+      return false;
+  });
+});
